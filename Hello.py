75808562b7d346a6c19e627a70348a031c1e2a4d
bt-png <110639339+bt-png@users.noreply.github.com>
2024-04-11 16:57:42 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -28,7 +28,7 @@ def run():
 
 def gsheets():
     conn = st.experimental_connection('gsheets', type=GSheetsConnection)
-    data = conn.read(worksheet='Attendee List (manual)')
+    data = conn.read(worksheet='1880233928')
     st.dataframe(data)
     
 def sample():
