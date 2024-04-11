6171c0595cf18b77341d437ccc17dfff147a6aef
bt-png <110639339+bt-png@users.noreply.github.com>
2024-04-11 16:11:03 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,28 +1,34 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
+from streamlit_gsheets import GSheetsConnection
 
 LOGGER = get_logger(__name__)
 
+def run():
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="👋",
+        initial_sidebar_state="collapsed",
+    )
+    Voting = st.expander('Open Working Group Chair Positions')
+    Voting.write('Nominate a candidate')
+    P1628 = Voting.container()
+    P1628.write("Vote Now")
+    P3357 = Voting.container()
+    P3357.write("Vote Now")
+    Attendance = st.expander('Record attendance')
+    AttendanceForm = Attendance.form(key='form_attendance')
+    col1, col2, col3 = AttendanceForm.columns([3, 1, 1])
 
+    lastname = col1.text_input(
+        'Last Name',
+        key='name',
+    )
+    AttendanceForm.form_submit_button(label="Submit")
 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-    initial_sidebar_state="collapsed",
-    
-)
-
+def sample():
+    st.write('test sample')
+if __name__ == '__main__':
+    run()
+elif __name__ == '__test__':
+    sample()
