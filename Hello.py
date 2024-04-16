6d0d31fc8b3c84b058e2fdec4f5b6b0dd7dfa4ec
bt-png <110639339+bt-png@users.noreply.github.com>
2024-04-15 17:03:43 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -17,16 +17,19 @@ if 'user_info' not in st.session_state:
     if do_you_have_an_account == 'Yes' and auth_form.form_submit_button(label='Sign In',use_container_width=True,type='primary'):
         with auth_notification, st.spinner('Signing in'):
             #auth_functions.sign_in(email,password)
+            st.success('SignedIn')
 
     # Create Account
     elif do_you_have_an_account == 'No' and auth_form.form_submit_button(label='Create Account',use_container_width=True,type='primary'):
         with auth_notification, st.spinner('Creating account'):
             #auth_functions.create_account(email,password)
+            st.success('AccountCreated')
 
     # Password Reset
     elif do_you_have_an_account == 'I forgot my password' and auth_form.form_submit_button(label='Send Password Reset Email',use_container_width=True,type='primary'):
         with auth_notification, st.spinner('Sending password reset link'):
             #auth_functions.reset_password(email)
+            st.success('Password Reset')
 
     # Authentication success and warning messages
     if 'auth_success' in st.session_state:
