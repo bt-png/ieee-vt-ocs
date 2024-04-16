cd01bc984c214976df51fcea05cf940b2c627ed7
bt-png <110639339+bt-png@users.noreply.github.com>
2024-04-16 01:03:39 +0000

    a

--- Hello.py
+++ Hello.py
@@ -1,4 +1,5 @@
-import streamlit as st
+import streamlit as st # pip install streamlit
+import auth_functions
 
 ## -------------------------------------------------------------------------------------------------
 ## Not logged in -----------------------------------------------------------------------------------
@@ -11,19 +12,19 @@ if 'user_info' not in st.session_state:
     auth_form = col2.form(key='Authentication form',clear_on_submit=False)
     email = auth_form.text_input(label='Email')
     password = auth_form.text_input(label='Password',type='password') if do_you_have_an_account in {'Yes','No'} else auth_form.empty()
+    firstname = auth_form.text_input(label='First Name') if do_you_have_an_account == 'No' else auth_form.empty()
+    lastname = auth_form.text_input(label='Last Name') if do_you_have_an_account == 'No' else auth_form.empty()
     auth_notification = col2.empty()
 
     # Sign In
     if do_you_have_an_account == 'Yes' and auth_form.form_submit_button(label='Sign In',use_container_width=True,type='primary'):
         with auth_notification, st.spinner('Signing in'):
-            #auth_functions.sign_in(email,password)
-            st.success('SignedIn')
+            auth_functions.sign_in(email,password)
 
     # Create Account
     elif do_you_have_an_account == 'No' and auth_form.form_submit_button(label='Create Account',use_container_width=True,type='primary'):
         with auth_notification, st.spinner('Creating account'):
-            #auth_functions.create_account(email,password)
-            st.success('AccountCreated')
+            auth_functions.create_account(email,password,firstname,lastname)
 
     # Password Reset
     elif do_you_have_an_account == 'I forgot my password' and auth_form.form_submit_button(label='Send Password Reset Email',use_container_width=True,type='primary'):
