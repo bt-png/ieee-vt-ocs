5bcab73cbf876e74d4018a42b6d058700cd21347
bt-png <110639339+bt-png@users.noreply.github.com>
2024-04-18 13:05:28 -0700

    Update streamlit_authentication.py

--- streamlit_authentication.py
+++ streamlit_authentication.py
@@ -39,10 +39,13 @@ def updateuser(auth,conf):
             st.sidebar.error(e)
 
 def register(auth, conf):
-    email, username, user = auth.register_user(location='sidebar', pre_authorization=False)
-    if email:
-        firestore.saveconfig(conf)
-    return email, username, user
+    try:
+        email, username, user = auth.register_user(location='sidebar', pre_authorization=False)
+        if email:
+            firestore.saveconfig(conf)
+        return email, username, user
+    except Exception as e:
+        st.sidebar.error(e)
 
 def forgotpassword(auth):
     return auth.forgot_password(location='sidebar')
