ae8fd9fe6fde1a3b8b45a69019c2579068e5a46c
bt-png <110639339+bt-png@users.noreply.github.com>
2024-04-22 17:28:05 +0000

    working groups

--- workinggroups.py
+++ workinggroups.py
@@ -3,15 +3,23 @@ import pandas as pd
 
 def run():
     st.subheader('Working Groups')
+    linkstruct = 'https://ieeexplore.ieee.org/search/searchresult.jsp?newsearch=true&contentType=standards&queryText='
     dfPARS = PARS()
     dfPARS = dfPARS[dfPARS['Project Status'] == 'Draft Development']
+    dfPARS['Link'] = linkstruct + dfPARS['Project Number'].astype(str)
     dfSt = Standards()
+    dfSt['Link'] = linkstruct + dfSt['Standard Number'].astype(str)
     st.caption('Active PARS')
     st.dataframe(data=dfPARS,
                  hide_index=True,
                  height = ((min(len(dfPARS.index),5) + 1) * 35 + 3),
-                 column_order=['Project Number', 'Expiration PAR Date', 'Project Title'],
+                 column_order=['Link', 'Project Number', 'Expiration PAR Date', 'Project Title'],
                  column_config={
+                     'Link': st.column_config.LinkColumn(
+                         label='',
+                         display_text='IEEE Site',
+                         width='small'
+                     ),
                      'Project Number': 'Ref',
                      'Expiration PAR Date': st.column_config.DateColumn(
                          label='Expiration',
@@ -25,8 +33,13 @@ def run():
     st.dataframe(data=dfSt.style.apply(color_coding, axis=1),
                  hide_index=True,
                  height = ((min(len(dfSt.index),5) + 1) * 35 + 3),
-                 column_order=['Standard Number', 'Year', 'Project Title', 'Status'],
+                 column_order=['Link', 'Standard Number', 'Year', 'Project Title', 'Status'],
                  column_config={
+                     'Link': st.column_config.LinkColumn(
+                         label='',
+                         display_text='IEEE Site',
+                         width='small'
+                     ),
                      'Standard Number': st.column_config.TextColumn(label='Ref'),
                      'Year': st.column_config.TextColumn(label='Published')
                  },
