5970c0096e51a592d6c71b5db75a2f381422acac
bt-png <110639339+bt-png@users.noreply.github.com>
2024-06-05 11:34:43 -0700

    Update voting_tally.py

--- voting_tally.py
+++ voting_tally.py
@@ -67,6 +67,32 @@ def nominations():
                     else:
                         st.caption(f"Your nomination has been revised, '{st.session_state.P3357_name}' has been entered")
                     show_nominations(nomination_df)
+    with st.expander('Nominate a candidate for P3425'):
+        title = workinggroups.PARS_Title('P3425')
+        scope = workinggroups.PARS_Scope('P3425')
+        st.caption('Title: ' + title)
+        st.caption('Scope: ' + scope)
+        with st.form(key='Nomination form (P3425)',clear_on_submit=False):
+            existing_vote = firestore.get_existing_nomination('P3425')
+            st.selectbox(
+                label = 'WG Chair Nominee Full Name',
+                options = roster.names(),
+                index=None,
+                placeholder= "Select nominee..." if existing_vote == '' else existing_vote,
+                key='P3425_name')
+            vote_label = 'Submit' if existing_vote == '' else 'Change Vote'
+            if st.form_submit_button(label=vote_label,use_container_width=False,type='primary'):
+                if st.session_state.P3425_name == st.session_state['name']:
+                    st.error('You cannot nominate yourself!')
+                elif st.session_state.P3425_name == None:
+                    st.warning('Please type a nominee...')
+                else:
+                    nomination_df = firestore.submit_nomination(st.session_state.P3425_name, 'P3425')
+                    if existing_vote == '':
+                        st.caption(f"Your nomination for '{st.session_state.P3425_name}' has been entered")
+                    else:
+                        st.caption(f"Your nomination has been revised, '{st.session_state.P3425_name}' has been entered")
+                    show_nominations(nomination_df)
 
 
 #@st.cache_data
