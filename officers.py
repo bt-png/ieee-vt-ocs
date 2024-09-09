28fc977820176a244dfeaffdc94d3544185ad87d
bt-png <110639339+bt-png@users.noreply.github.com>
2024-09-09 11:46:54 -0400

    Update officers.py

--- officers.py
+++ officers.py
@@ -9,4 +9,6 @@ def run():
                 "Media Chair - Eric Parsons - [eric.parsons@southwire.com](mailto:eric.parsons@southwire.com)  \n"
                 "[Contact the Officers](mailto:stephen-norton@ieee.org;jschlick@hntb.com;heather.riebeling@aecom.com;brett.tharp@stvinc.com;eric.parsons@southwire.com)"
     )
+    st.page_link(page='https://vtsociety.org/committee/overhead-contact-systems-standards-committee', label='IEEE VT/OCS')
     st.page_link(page='https://ieee-sa.imeetcentral.com/vts-ocs-sc/home', label='IEEE SA Committee Workspace')
+    
