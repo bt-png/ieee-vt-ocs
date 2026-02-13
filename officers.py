2d618f7a76275fa9d318c60dcdc9da149de4ad2b
bt-png <110639339+bt-png@users.noreply.github.com>
2026-02-13 08:28:59 -0800

    Update officers.py

--- officers.py
+++ officers.py
@@ -3,17 +3,17 @@ import streamlit as st
 
 def officerlist():
     # Username of Officers
-    return ['btharp', 'schlick33', 'ucme4me', 'iccswusn', 'hriebeling', 'proder']
+    return ['btharp', 'schlick33', 'ucme4me', 'hriebeling']
 
 
 def run():
     st.caption(
-                "In Memoriam - Steve Norton  \n"
-                "interim Chairperson, Vice Chairperson - John Schlick - [jschlick@hntb.com](mailto:jschlick@hntb.com)  \n"
+                "Chairperson - John Schlick - [jschlick@hntb.com](mailto:jschlick@hntb.com)  \n"
+                "Vice Chairperson - OPEN\n"
                 "Secretary - Heather Riebeling - [heather.riebeling@aecom.com](mailto:heather.riebeling@aecom.com)  \n"
                 "Recording Secretary - Brett Tharp - [brett.tharp@stvinc.com](mailto:brett.tharp@stvinc.com)  \n"
                 "Media Chair - Eric Parsons - [eric.parsons@southwire.com](mailto:eric.parsons@southwire.com)  \n"
-                "[Contact the Officers](mailto:jschlick@hntb.com;heather.riebeling@aecom.com;brett.tharp@stvinc.com;eric.parsons@southwire.com)"
+                "[Contact the Officers](mailto:jschlick@hntb.com;heather.riebeling@aecom.com;brett.tharp@stvinc.com;eric.parsons@southwire.com;c.deshmukkh@ieee.org)"
     )
     st.page_link(page='https://vtsociety.org/committee/overhead-contact-systems-standards-committee', label='IEEE VT/OCS')
     st.page_link(page='https://ieee-sa.imeetcentral.com/vts-ocs-sc/home', label='IEEE SA Committee Workspace')
