c7d3498448aa1144c3f2ad3369cf8e24f633546f
bt-png <110639339+bt-png@users.noreply.github.com>
2026-06-30 12:44:26 -0700

    Update officers.py

--- officers.py
+++ officers.py
@@ -13,7 +13,7 @@ def run():
                 "Secretary - Heather Riebeling - [heather.riebeling@aecom.com](mailto:heather.riebeling@aecom.com)  \n"
                 "Recording Secretary - Brett Tharp - [brett.tharp@stvinc.com](mailto:brett.tharp@stvinc.com)  \n"
                 "Media Chair - Eric Parsons - [eric.parsons@southwire.com](mailto:eric.parsons@southwire.com)  \n"
-                "[Contact the Officers](mailto:jschlick@hntb.com;moisesramos@ieee.org;heather.riebeling@aecom.com;brett.tharp@stvinc.com;eric.parsons@southwire.com;allen.jennifer@ieee.org)"
+                "[Contact the Officers](mailto:jschlick@hntb.com;moisesramos@ieee.org;heather.riebeling@aecom.com;brett.tharp@stvinc.com;eric.parsons@southwire.com;allen.jennifer@ieee.org?subject=IEEE%20VT/OCS%20Standards%20Committee%20)"
     )
 
     st.page_link(page='https://vtsociety.org/committee/overhead-contact-systems-standards-committee', label='IEEE VT/OCS')
